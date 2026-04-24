08b055d205581bf86a991bc0e8a39095b98087c8
Anderson Porto <anderson.higo2@gmail.com>
2026-04-24 00:53:24 -0300

    feat: reorganize import statements in __main__.py for clarity

--- __main__.py
+++ __main__.py
@@ -3,12 +3,11 @@ import sys
 from pathlib import Path
 import asyncio
 
+from src.bot import main
+
 project_root = Path(__file__).parent
 src_path = project_root / "src"
 sys.path.insert(0, str(src_path))
 
-from src.bot import main
-
 if __name__ == "__main__":
     asyncio.run(main())
-
